1cd8d2f11ee5df37ec15fbf038214446c48243cc
Sharankumar Narayan Huggi <snarayanhuggi@apple.com>
2023-09-27 17:37:59 -0700

    Updated with straddle option

--- main.py
+++ main.py
@@ -4,14 +4,42 @@ import numpy as np
 import random
 import collections
 
-win_rate = 90/100
-rr_ratio = 1/3 # Reward/Risk ratio
-max_risk_per_trade = 5/100 # Max Loss as % of account size
-max_risk_all_trades = 15/100
-uncorrelated_parallel_trades = int(max_risk_all_trades/max_risk_per_trade)
-account_size = 20*1000
-average_trade_duration = 14
+## Current Setup of ~10 delta strangles
+#win_rate = 90/100
+#rw_to_cr_ratio = 1/2 # 50% profit target
+#cr_to_sl_ratio = 1/1.5 # 150% SL of the credit received
+#rr_ratio = rw_to_cr_ratio*cr_to_sl_ratio # Reward/Risk ratio
+#max_risk_per_trade = 5/100 # Max Loss as % of account size
+#max_risk_all_trades = 15/100
+#uncorrelated_parallel_trades = int(max_risk_all_trades/max_risk_per_trade)
+#account_size = 20*1000
+#average_trade_duration = 14
 
+## Higher delta of ~20 delta strangles
+#win_rate = 80/100
+#rw_to_cr_ratio = 1/2 # 50% profit target
+#cr_to_sl_ratio = 1/1 # 150% SL of the credit received
+#rr_ratio = rw_to_cr_ratio*cr_to_sl_ratio # Reward/Risk ratio
+#max_risk_per_trade = 5/100 # Max Loss as % of account size
+#max_risk_all_trades = 15/100
+#uncorrelated_parallel_trades = int(max_risk_all_trades/max_risk_per_trade)
+#account_size = 20*1000
+#average_trade_duration = 14
+
+# Straddle setup 
+win_rate = 50/100
+rw_to_cr_ratio = 0.5/1 # 50% profit target
+cr_to_sl_ratio = 0.3/1 # 100% SL of the credit received
+rr_ratio = rw_to_cr_ratio/cr_to_sl_ratio
+max_risk_per_trade = 3/100
+max_risk_all_trades = 10/100
+uncorrelated_parallel_trades = int(max_risk_all_trades/max_risk_per_trade)
+account_size = 10000
+average_trade_duration = 14
+print("Straddle Parameters")
+print("Reward to Credit Ratio : {0:.2f}%".format(rw_to_cr_ratio*100))
+print("Credit to SL Ratio : {0:.2f}%".format(cr_to_sl_ratio*100))
+print("Reward/Risk Ratio : {0:.2f}%".format(rr_ratio*100))
 
 def get_account_samples(seed, win_rate, rr_ratio, max_risk, account_size, average_trade_duration, parallel_trades):
     trades_count = int(365/average_trade_duration)
@@ -41,7 +69,7 @@ def ascii_histogram(seq) -> None:
     """A horizontal frequency-table/histogram plot."""
     counted = collections.Counter(seq)
     for k in sorted(counted):
-        print('{0:5d} {1}'.format(k, '+' * counted[k]))
+        print('{0:5d}% {1}'.format(k, '+' * counted[k]))
 
 trial_count = 100
 
@@ -49,11 +77,12 @@ annual_gain_average = 0
 annual_gains = []
 for seed in range(trial_count):
     [account_value, account_samples, trades_by_symbol] = get_account_samples(seed, win_rate, rr_ratio, max_risk_per_trade, account_size, average_trade_duration, uncorrelated_parallel_trades)
-    print("Annual Gain : {0:.1f}".format((account_value-account_size)/account_size*100))
+    print("Annual Gain : {0:.1f}%".format((account_value-account_size)/account_size*100))
     annual_gain = int((account_value-account_size)/account_size*100)
     annual_gains.append(annual_gain)
     annual_gain_average += annual_gain
 
+print("Number of parallel trades : {0}".format(uncorrelated_parallel_trades))
 ascii_histogram(annual_gains)
 
 #print("Annaul Gain Average : {0:.2f}".format(annual_gain_average/trial_count))
